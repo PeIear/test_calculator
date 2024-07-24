5b9118367cce0b417e6649aabc7b37b23122d40c
Yarche <yaroslav_petrochenko@inbox.ru>
2024-07-24 15:15:08 +0300

    forth_commit

--- test_calculator.py
+++ test_calculator.py
@@ -1,41 +1,106 @@
+import test_vallue
+import decimal
 
+# Основная функция выполнения всех реализованных операций
 
-# import math
-
-def aritmetic(int_1, operation, int_2):
+def aritmetic(float_1, operation, float_2):
     if operation == '+':
+        value_1 = test_vallue.value_chek(float_1)
+        value_2 = test_vallue.value_chek(float_2)
+        value_result = test_vallue.value_chek(decimal.Decimal(str(float_1)) + decimal.Decimal(str(float_2)))
         result = f"""
+
+Выражение:
+{value_1} {operation} {value_2}
+
 Результат:
-{int_1 + int_2}"""
+{value_result}"""
+
+        return result
+
     elif operation == '-':
+        value_1 = test_vallue.value_chek(float_1)
+        value_2 = test_vallue.value_chek(float_2)
+        value_result = test_vallue.value_chek(decimal.Decimal(str(float_1)) - decimal.Decimal(str(float_2)))
         result = f"""
+
+Выражение:
+{value_1} {operation} {value_2}
+
 Результат:
-{int_1 - int_2}"""
+{value_result}"""
+
+        return result
+
     elif operation == '/':
-        if int_2 == 0:
-            result = f"На ноль делить нельзя!"
+        value_1 = test_vallue.value_chek(float_1)
+        value_2 = test_vallue.value_chek(float_2)
+        value_result = test_vallue.value_chek(decimal.Decimal(str(float_1)) / decimal.Decimal(str(float_2)))
+        if float_2 == 0:
+            result = f"""
+
+Выражение:
+{value_1} {operation} {value_2}
+
+На ноль делить нельзя!"""
         else:
             result = f"""
+
+Выражение:
+{value_1} {operation} {value_2}
+
 Результат:
-{int_1 / int_2}"""
+{value_result}"""
+
+        return result
+
     elif operation == '*':
+        value_1 = test_vallue.value_chek(float_1)
+        value_2 = test_vallue.value_chek(float_2)
+        value_result = test_vallue.value_chek(decimal.Decimal(str(float_1)) * decimal.Decimal(str(float_2)))
         result = f"""
+
+Выражение:
+{value_1} {operation} {value_2}
+
 Результат:
-{int_1 * int_2}"""
-    elif operation == '**':
+{value_result}"""
+
+        return result
+
+    elif operation == 'pow':
+        value_1 = test_vallue.value_chek(float_1)
+        value_2 = test_vallue.value_chek(float_2)
+        if float_1 >= 0:
+            value_result = test_vallue.value_chek(decimal.Decimal(str(float_1)) ** decimal.Decimal(str(float_2)))
+        else:
+            value_result = test_vallue.value_chek((float_1 ** float_2))
         result = f"""
+        
+Выражение:
+{value_1} {operation} {value_2}
+Число {value_1} в {value_2}-й степени
+
 Результат:
-{int_1 ** int_2}"""
+{value_result}"""
+
+        return result
+
     elif operation == 'root':
-        result = (f"""
+        value_1 = test_vallue.value_chek(float_1)
+        value_2 = test_vallue.value_chek(float_2)
+        if float_1 >= 0:
+            value_result = test_vallue.value_chek(decimal.Decimal(str(float_1)) ** decimal.Decimal(decimal.Decimal(str(1))/decimal.Decimal(str(float_2))))
+        else:
+            value_result = test_vallue.value_chek((float_1 ** (1/float_2)))
+        result = f"""
+        
+Выражение:
+{value_1} {operation} {value_2}
+{value_2}(th) {operation} of a number {value_1}
+Корень {value_2}-й степени из числа {value_1}
+
 Результат:
-{int_1 ** (1/int_2)}""")
+{value_result}"""
 
-
-    return result
-
-
-
-
-
-
+        return result
---
+++ test_vallue.py
@@ -0,0 +1,48 @@
+import re
+import test_calculator
+
+
+positive_float = 'Число с плавающей точкой'
+negative_float = 'Отрицательное число с плавающей точкой'
+positive_integer = 'Целое положительное число'
+negative_integer = 'Целое Отрицательное число'
+
+
+# Функция для определения типа входных значений (чисел)
+
+def check_int(int):
+    if re.fullmatch(r'[+]?\d+\.[1-9]\d*', str(int)) != None:
+        result = positive_float
+        # return False
+        return result
+    elif re.fullmatch(r'[-]\d+\.[1-9]\d*', str(int)) != None:
+        return negative_float
+    elif re.fullmatch(r'[-]\d+\.[0]+', str(int)) != None:
+        return negative_integer
+    elif re.fullmatch(r'[+]?\d+\.[0]+', str(int)) != None:
+        return positive_integer
+
+
+
+# Функция для грамотного и удобного отображения на экран пользователя результата - значений (чисел)
+# с определённым типом данных (в функции выше)
+
+def value_chek(int_test):
+    if check_int(int_test) == positive_integer:
+        value = int(int_test)
+        return value
+    elif check_int(int_test) == negative_float:
+        if type(int_test) == complex:
+            value = f"{int_test}"
+        else:
+            value = f"({float(int_test)})"
+        return value
+    elif check_int(int_test) == negative_integer:
+        value = f"({int(int_test)})"
+        return value
+    else:
+        if type(int_test) == complex:
+            value = f"{int_test}"
+        else:
+            value = f"{float(int_test)}"
+        return value
